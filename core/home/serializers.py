7dde46e0642ce99d7394af59457bce745876fda2
Gaurav-714 <gauravnagose714@gmail.com>
2024-05-07 16:49:51 +0530

    Signup and Login

---
+++ core/home/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.EmailField()
+
+    def validate(self, data):
+        user = User.objects.filter(username = data['username'])
+        if user.exists():
+            raise serializers.ValidationError('Username is already taken.')
+        return data
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data['username'].lower(),
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return validated_data
+    
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = User.objects.filter(username=data['username'])
+        if not user.exists():
+            raise serializers.ValidationError('Account not found.')
+        return data
+    
+    def get_jwt_token(self, validated_data):
+        user = authenticate(username=validated_data['username'], password=validated_data['password'])
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return {'message':'Logged in successfully.', 'data':{'token':{'refresh':str(refresh), 'access':str(refresh.access_token)}}}
+        else:
+            return {'message':'Invalid credentials.', 'data':{}}
--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .serializers import *
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = RegisterSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'success' : True,
+                    'message' : 'Your account is created.',
+                    'data' : serializer.data
+                }, status = status.HTTP_201_CREATED)
+            return Response({
+                    'success' : False,
+                    'message' : 'Something went wrong.',
+                    'error' : serializer.errors
+                }, status = status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({
+                    'success' : False,
+                    'message' : 'Something went wrong.',
+                    'error' : serializer.errors
+                }, status = status.HTTP_400_BAD_REQUEST)
+        
+
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = LoginSerializer(data=data)
+            if serializer.is_valid():
+                response = serializer.get_jwt_token(serializer.validated_data)
+                return Response(response, status=status.HTTP_200_OK)
+            return Response({
+                    'success' : False,
+                    'message' : 'Error occured.',
+                    'error' : serializer.errors
+                }, status = status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            print(ex)
+            return Response({
+                    'success' : False,
+                    'message' : 'Something went wrong..',
+                }, status = status.HTTP_400_BAD_REQUEST)
+
